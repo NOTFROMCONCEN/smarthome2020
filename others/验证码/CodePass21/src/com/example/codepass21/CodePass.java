6a3e44db0d287dc24d948f05de91ab9dddf50f93
奶油话梅糖 <1097681347@qq.com>
2020-06-09 11:18:05 +0800

    20200609

---
+++ others/验证码/CodePass21/src/com/example/codepass21/CodePass.java
@@ -0,0 +1,50 @@
+package com.example.codepass21;
+
+import java.util.Random;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Bitmap.Config;
+
+public class CodePass {
+
+	public static String code = "";
+	public static String all_code = "58t4e8r8ku64lkiu647885a6w84fg857WQ95JN48S64B85ED4G86";
+
+	public static Bitmap createBitmap() {
+		Random random = new Random();
+		Bitmap bitmap = Bitmap.createBitmap(140, 50, Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		Paint paint = new Paint();
+		paint.setTextSize(20);
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < 4; i++) {
+			stringBuffer.append(all_code.charAt(random.nextInt(all_code
+					.length())));
+		}
+		code = stringBuffer.toString();
+		int base_left = 20;
+		int base_top = 20;
+		int random_left = 23;
+		int random_top = 20;
+		for (int i = 0; i < code.length(); i++) {
+			paint.setFakeBoldText(random.nextBoolean());
+			paint.setTextSkewX(-random.nextInt(1));
+			paint.setColor(Color.rgb(random.nextInt(256), random.nextInt(256),
+					random.nextInt(256)));
+			random_top = base_top + random.nextInt(10);
+			canvas.drawText(code.charAt(i) + "", base_left, random_top, paint);
+			base_left += random_left + random.nextInt(10);
+		}
+		canvas.drawLine(random.nextInt(canvas.getWidth()),
+				random.nextInt(canvas.getHeight()),
+				random.nextInt(canvas.getWidth()),
+				random.nextInt(canvas.getHeight()), paint);
+		canvas.save(Canvas.ALL_SAVE_FLAG);
+		canvas.restore();
+		return bitmap;
+	}
+
+}
