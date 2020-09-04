e293bef781d7a7d9b2d9124a39351b78c29fb8e6
奶油话梅糖 <1097681347@qq.com>
2020-09-04 20:45:10 +0800

    20200904

---
+++ 源码文件/赛题源码/2018年省赛题库/无注释/2018省赛样卷0904/src/com/example/shengsaiyangjuan20180904/fragment/BarActivity.java
@@ -0,0 +1,123 @@
+package com.example.shengsaiyangjuan20180904.fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.bizideal.smarthome.socket.ConstantUtil;
+import com.bizideal.smarthome.socket.ControlUtils;
+import com.bizideal.smarthome.socket.LoginCallback;
+import com.bizideal.smarthome.socket.SocketClient;
+import com.example.shengsaiyangjuan20180904.LoginActivity;
+import com.example.shengsaiyangjuan20180904.R;
+import com.example.shengsaiyangjuan20180904.tools.DiyToast;
+
+public class BarActivity extends FragmentActivity {
+
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
+	private List<Fragment> faFragments = new ArrayList<Fragment>();
+
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
+	boolean web_state = true;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bar);
+		faFragments.add(new BaseFragment());
+		faFragments.add(new LinkFragment());
+		faFragments.add(new UpdataPassFragment());
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager(), faFragments);
+
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+		ControlUtils.setUser("bizideal", "123456",
+				LoginActivity.sharedPreferences.getString("ip", null));
+		SocketClient.getInstance().creatConnect();
+		SocketClient.getInstance().login(new LoginCallback() {
+
+			@Override
+			public void onEvent(final String arg0) {
+				// TODO Auto-generated method stub
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						if (arg0.equals(ConstantUtil.FAILURE)) {
+							DiyToast.showToast(getApplicationContext(), "连接失败");
+						} else if (arg0.equals(ConstantUtil.SUCCESS)) {
+							DiyToast.showToast(getApplicationContext(), "连接成功");
+						} else {
+							DiyToast.showToast(getApplicationContext(), "未知环境");
+						}
+					}
+				});
+			}
+		});
+
+	}
+
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		private List<Fragment> fragments;
+
+		public SectionsPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+			super(fm);
+			this.fragments = fragments;
+		}
+
+		@Override
+		public int getCount() {
+			// Show 3 total pages.
+			return fragments.size();
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+			case 0:
+				return getString(R.string.title_section1).toUpperCase();
+			case 1:
+				return getString(R.string.title_section2).toUpperCase();
+			case 2:
+				return getString(R.string.title_section3).toUpperCase();
+			}
+			return null;
+		}
+
+		@Override
+		public Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return fragments.get(arg0);
+		}
+	}
+}
